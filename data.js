dac238619f4147146fc909a016b6343beab9d908
Nishanth <106056452+tejonish@users.noreply.github.com>
2023-12-13 14:03:40 +0530

    react js

---
+++ data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: 'TEJ',
+        age: 22,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+        id: 2,
+        name: 'MEERA',
+        age: 42,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+        id: 3,
+        name: 'BABU',
+        age: 54,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+        id: 4,
+        name: 'NISH',
+        age: 20,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    
+];
